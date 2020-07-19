9ad2a43da6480869feb8269473f1420f3dbbcccd
DavidPetkovsek <d.petkovsek@outlook.com>
2020-07-19 19:18:39 -0400

    clean up

--- deepPassage.py
+++ deepPassage.py
@@ -35,13 +35,13 @@ xs[-1] = layers.Conv2DTranspose(filters=64, kernel_size=3, strides=1, padding='s
 xs[-1] = layers.Conv2DTranspose(filters=128, kernel_size=3, strides=1, padding='same', activation='relu')(xs[-1])
 xs[-1] = layers.Conv2DTranspose(filters=256, kernel_size=3, strides=1, padding='same', activation='relu')(xs[-1])
 xs[-1] = layers.Reshape((256, 256, 1))(xs[-1]) # reshape to proper shape for StyleGAN2
-xs[0] = layers.Reshape(tuple([512]))(xs[0]) # reshape to proper shape for StyleGAN2
-xs[1] = layers.Reshape(tuple([512]))(xs[1]) # reshape to proper shape for StyleGAN2
-xs[2] = layers.Reshape(tuple([512]))(xs[2]) # reshape to proper shape for StyleGAN2
-xs[3] = layers.Reshape(tuple([512]))(xs[3]) # reshape to proper shape for StyleGAN2
-xs[4] = layers.Reshape(tuple([512]))(xs[4]) # reshape to proper shape for StyleGAN2
-xs[5] = layers.Reshape(tuple([512]))(xs[5]) # reshape to proper shape for StyleGAN2
-xs[6] = layers.Reshape(tuple([512]))(xs[6]) # reshape to proper shape for StyleGAN2
+xs[0] = layers.Reshape([512])(xs[0]) # reshape to proper shape for StyleGAN2
+xs[1] = layers.Reshape([512])(xs[1]) # reshape to proper shape for StyleGAN2
+xs[2] = layers.Reshape([512])(xs[2]) # reshape to proper shape for StyleGAN2
+xs[3] = layers.Reshape([512])(xs[3]) # reshape to proper shape for StyleGAN2
+xs[4] = layers.Reshape([512])(xs[4]) # reshape to proper shape for StyleGAN2
+xs[5] = layers.Reshape([512])(xs[5]) # reshape to proper shape for StyleGAN2
+xs[6] = layers.Reshape([512])(xs[6]) # reshape to proper shape for StyleGAN2
 
 sg = StyleGAN2()
 # sg.load(28)
@@ -63,25 +63,3 @@ model.fit(train_data_multi, epochs=EPOCHS,
                           steps_per_epoch=EVALUATION_INTERVAL,
                           validation_data=val_data_multi,
                           validation_steps=50)
-
-# def preprocess_image(image):
-#     return image
-#
-# def load_and_preprocess_image(path):
-#     # image = read image from path
-#     # return preprocess_image(image)
-#     pass
-#
-# path_ds = tf.data.Dataset.from_tensor_slices(files)
-# image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
-# label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(categories, tf.int64))
-# image_label_ds = tf.data.Dataset.zip((image_ds, label_ds))
-#
-# ds = image_label_ds.shuffle(buffer_size=1000 * BATCH_SIZE)
-# ds = ds.repeat()
-# ds = ds.batch(BATCH_SIZE)
-# # `prefetch` lets the dataset fetch batches, in the background while the model is training.
-# ds = ds.prefetch(buffer_size=AUTOTUNE)
-#
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-# model.fit(ds, epochs=3, steps_per_epoch=10)
